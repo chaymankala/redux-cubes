8a34980a3d6f34d37926750142094e501fb7f3a5
chaymankala <chaithanya.mankala@gmail.com>
2018-08-23 01:04:20 +0530

    [update] cubes func

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { toggleCubes } from '../actions';
+import store from '../store';
 
 export default class App extends Component {
-  render() {
-    return (
-      <div>React simple starter</div>
-    );
-  }
+
+    CUBE_CLASS = "col-md-6 col-6 cube";
+
+    CUBE = (color) => {
+        return <div key={color} className={`${this.CUBE_CLASS} ${color}`} onClick={() => { store.dispatch(toggleCubes()) }}></div>
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            cubes: store.getState().cubes.cubes || []
+        }
+    }
+    componentWillMount() {
+        store.subscribe(() => {
+            this.setState({ cubes: store.getState().cubes.cubes });
+        });
+    }
+    render() {
+        var { cubes } = this.state;
+        return (
+            <div>
+                {cubes && cubes.length ? cubes.map((color) => { return this.CUBE(color) }) : null}
+            </div>
+        );
+    }
 }
